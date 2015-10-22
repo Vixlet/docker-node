5a5ea9c6d5b8798dd0909d0a2b34b98af2f5ae98
Matt Lubner <matt@mattlubner.com>
2015-10-21 22:14:09 -0700

    fix: start.sh now correctly aborts on failures

--- start.sh
+++ start.sh
@@ -2,20 +2,20 @@
 
 
 # Set default value for pre-start script environment variable
-if [ -z "${DOCKER_PRESTART_SCRIPT}" ]; then
+if [ $? -eq 0 ] && [ -z "${DOCKER_PRESTART_SCRIPT}" ]; then
   DOCKER_PRESTART_SCRIPT="./docker-prestart.sh"
 fi
 
 
 # Run user-specified pre-start script, if it exists
-if [ -x ${DOCKER_PRESTART_SCRIPT} ]; then
-  test $? && echo "Running docker prestart script..." \
+if [ $? -eq 0 ] && [ -x ${DOCKER_PRESTART_SCRIPT} ]; then
+  echo "Running docker prestart script..." \
     && ${DOCKER_PRESTART_SCRIPT}
 fi
 
 
 # Install dependencies?
-if [ -z "${DOCKER_PREINSTALLED}" ]; then
-  test $? && echo "Installing NPM dependencies..." \
+if [ $? -eq 0 ] && [ -z "${DOCKER_PREINSTALLED}" ]; then
+  echo "Installing production NPM dependencies..." \
     && npm install --production
 fi
