e885e705ebfb2634611481f81a45f59236956f8b
Matt Lubner <matt@mattlubner.com>
2016-01-05 16:42:31 -0800

    simplified start.sh

--- start.sh
+++ start.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
+set -e
+
+
+# Default values for environment variables
+DOCKER_PRESTART_SCRIPT="${DOCKER_PRESTART_SCRIPT:-./docker-prestart.sh}"
 
 
 # Install dependencies?
-if [ $? -eq 0 ] && [ -z "${DOCKER_PREINSTALLED}" ]; then
-  echo "Installing production NPM dependencies..." \
-    && npm install --production --no-bin-links
-fi
-
-
-# Set default value for pre-start script environment variable
-if [ $? -eq 0 ] && [ -z "${DOCKER_PRESTART_SCRIPT}" ]; then
-  DOCKER_PRESTART_SCRIPT="./docker-prestart.sh"
+if [ -z "${DOCKER_PREINSTALLED}" ]; then
+  echo "Installing production NPM dependencies..."
+  npm install --production --no-bin-links
 fi
 
 
 # Run user-specified pre-start script, if it exists
-if [ $? -eq 0 ] && [ -x ${DOCKER_PRESTART_SCRIPT} ]; then
-  echo "Running docker prestart script..." \
-    && ${DOCKER_PRESTART_SCRIPT}
+if [ -x ${DOCKER_PRESTART_SCRIPT} ]; then
+  echo "Running docker prestart script..."
+  ${DOCKER_PRESTART_SCRIPT}
 fi
 
 
 # Start the container
-if [ $? -eq 0 ] && [ "${#}" -lt 1 ]; then
+if [ "${#}" -lt 1 ]; then
   echo "Starting Node (via \`npm start\`)..."
   exec npm start
-elif [ $? -eq 0 ]; then
+else
   exec "${@}"
 fi
