8b9ed3e53ccd3db443107b14cc7330f86f2124e3
Matt Lubner <matt@mattlubner.com>
2016-01-12 14:01:50 -0800

    fix for projects w/o prestart script

--- start.sh
+++ start.sh
@@ -3,21 +3,25 @@ set -e
 
 
 # Default values for environment variables
-CONTAINER_PRESTART="${CONTAINER_PRESTART:-./docker-prestart.sh}"
-CONTAINER_PREINSTALL="${CONTAINER_PREINSTALL:-npm install --production --no-bin-links}"
+DEFAULT_CONTAINER_PREINSTALL="npm install --production --no-bin-links"
+DEFAULT_CONTAINER_PRESTART="./docker-prestart.sh"
 
 
 # Install dependencies?
 if [ "${CONTAINER_PREINSTALL}" != "skip" ]; then
   echo "Installing production NPM dependencies..."
-  ${CONTAINER_PREINSTALL}
+  ${CONTAINER_PREINSTALL:-$DEFAULT_CONTAINER_PREINSTALL}
 fi
 
 
-# Run user-specified pre-start script, if it exists
-if [ -n ${CONTAINER_PRESTART} ]; then
-  echo "Running docker prestart script..."
+# Run user-specified pre-start command
+if [ -n "${CONTAINER_PRESTART}" ]; then
+  echo "Running custom prestart command..."
   ${CONTAINER_PRESTART}
+# Run pre-start script, if it exists
+elif [ -e "${DEFAULT_CONTAINER_PRESTART}" ] && [ -x "${DEFAULT_CONTAINER_PRESTART}" ]; then
+  echo "Running prestart script..."
+  ${DEFAULT_CONTAINER_PRESTART}
 fi
 
 
