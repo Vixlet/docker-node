14b1873dfa6e719951889cd4c103055d897e2459
Matt Lubner <matt@mattlubner.com>
2016-02-24 15:58:24 -0800

    test: use --no-cache with Dockerfiles

--- test/5/test.sh
+++ test/5/test.sh
@@ -5,5 +5,5 @@ rm -rf node_modules
 rm -f "Dockerfile.${VERSION}"
 sed 's/\${VERSION}/'${VERSION}'/g' Dockerfile > "Dockerfile.${VERSION}"
 npm install --production
-docker build -f "Dockerfile.${VERSION}" -t "vixlet-node-test-4-image" .
+docker build --no-cache -f "Dockerfile.${VERSION}" -t "vixlet-node-test-4-image" .
 docker run -t --rm --name "vixlet-node-test-4" "vixlet-node-test-4-image"
