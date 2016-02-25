ecc64e3fb9bc670132cd41899716aecb68b024d6
Matt Lubner <matt@mattlubner.com>
2016-02-24 19:36:01 -0800

    chore: better logging for travis!

--- tools/make.sh
+++ tools/make.sh
@@ -11,6 +11,6 @@ source ./tools/resolve_envs.sh
 rm -rf ./${NODE_VERSION_PATH}
 mkdir -p ./${NODE_VERSION_PATH}
 cp -r ./context/ ./${NODE_VERSION_PATH}/
-echo "make: ls of pwd: $(ls -la)"
-echo "make: ls of NODE_VERSION_PATH: $(ls -la ./${NODE_VERSION_PATH}/)"
 sed 's/\${NODE_VERSION}/'"${NODE_VERSION}"'/g' ./Dockerfile > ./${NODE_VERSION_PATH}/Dockerfile
+echo -e "make: ls $(pwd):\n$(ls -la)"
+echo -e "make: ls $NODE_VERSION_PATH:\n$(ls -la ./${NODE_VERSION_PATH}/)"
