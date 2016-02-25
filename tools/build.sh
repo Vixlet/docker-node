0864f3ddeecaa2dcaf9e2eb287c42fcd72e517fd
Matt Lubner <matt@mattlubner.com>
2016-02-24 19:33:08 -0800

    chore: build logs are redundant

--- tools/build.sh
+++ tools/build.sh
@@ -8,6 +8,4 @@ set -e
 echo "build: pwd: $(pwd)"
 source ./tools/versions.env
 source ./tools/resolve_envs.sh
-echo "build: ls: $(ls -la)"
-echo "build: ls of NODE_VERSION_PATH: $(ls -la ./${NODE_VERSION_PATH}/)"
 docker build --no-cache -f ./${NODE_VERSION_PATH}/Dockerfile -t "vixlet/node:${IMAGE_NODE_VERSION}" ./${NODE_VERSION_PATH}
