ac64ed3ba1e5cd492a826c1bbfe0df63a002f630
Matt Lubner <matt@mattlubner.com>
2016-02-24 19:28:44 -0800

    chore: more logging 'cuz travis hates trees!

--- tools/make.sh
+++ tools/make.sh
@@ -11,4 +11,6 @@ source ./tools/resolve_envs.sh
 rm -rf ./${NODE_VERSION_PATH}
 mkdir -p ./${NODE_VERSION_PATH}
 cp -r ./context/ ./${NODE_VERSION_PATH}/
+echo "make: ls of pwd: $(ls -la)"
+echo "make: ls of NODE_VERSION_PATH: $(ls -la ./${NODE_VERSION_PATH}/)"
 sed 's/\${NODE_VERSION}/'"${NODE_VERSION}"'/g' ./Dockerfile > ./${NODE_VERSION_PATH}/Dockerfile
